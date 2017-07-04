d17ca79fb675868aa5dcadaa32cff21ad6227de7
Taylor Rhodes <taylorsingletonfookes@live.com>
2017-07-04 15:34:51 -0700

    handle double tap errors better

--- bountyChecker.js
+++ bountyChecker.js
@@ -1,7 +1,7 @@
 const request = require('superagent')
 const config = require('./config')
 
-var claimRequest, payoutRequest, activeBounty
+var claimRequest, payoutRequest, activeBounty, claimant
 function resetBountyClaim(){
   claimRequest  = {
     action: {
@@ -18,82 +18,90 @@ function resetBountyClaim(){
 }
 resetBountyClaim()
 
-function bountyClaimProcess(scannedFob, isHandledCallback) {
-  if (activeBounty) {
-      // Have an active bounty so next tap is to claim bounty:
-      request
-          .get(config.brainLocation + 'members/' + scannedFob)
-          .end((err, res) => {
-            if (err || res.body.error) {
-              console.log('Invalid Fob')
-              // clear bounty if random fob tries to claim?
-              resetBountyClaim()
-              return isHandledCallback(false)
+function bountyTagCheck(fob, isHandledCallback){
+    request
+        .get(config.brainLocation + 'bounties/' + scannedFob)
+        .end((err, res) => {
+            if ( err || res.body.error ) {
+                console.log('Invalid Fob, bounties/:fob')
             }
 
-            payoutRequest.action["address"] = res.body.address
-            claimRequest.action["address"] = res.body.address
-            claimRequest.action["notes"] = Date.now().toString()
+            activeBounty = res.body
+            console.log("new active bounty!", activeBounty)
 
-            console.log({payoutRequest, claimRequest})
-
-            request
-                .post(config.brainLocation + 'members')
-                .send(payoutRequest)
-                .end((err, res) => {
-                    if (err || res.body.error) {
-                      console.log('Unable to create')
-                      return null
-                    }
-                    console.log(res.body)
-                })
-
-            request
-                .post(config.brainLocation + 'bounties')
-                .send(claimRequest)
-                .end((err, res) => {
-                    if (err || res.body.error) {
-                      console.log('Unable to create')
-                    }
-                    console.log(res.body)
-                })
-
-            request
-                .post(config.bountiesSlack)
-                .send({text: activeBounty.name + ' was claimed by ' + res.body.name+ ' for $'+ payoutRequest.action.amount})
-                .end( (err, res)=> {
-                    console.log({err,res})
-                })
-
-
-            resetBountyClaim()
+            let now = Date.now()
+            let monthValue = activeBounty.value
+            let lastClaimed = activeBounty.notes
+            let amount = calculatePayout(monthValue, lastClaimed, now)
+            // Build in the info we need from the bounty, next tap will send these requests
+            claimRequest.action["bounty-id"] = activeBounty["bounty-id"]
+            payoutRequest.action["notes"] = activeBounty["bounty-id"]
+            payoutRequest.action["amount"] = amount.toString()
+            // This was a bounty tag so we do not need to check for beer
             isHandledCallback(true)
-
-          })
-  } else {
-    request
-      .get(config.brainLocation + 'bounties/' + scannedFob)
-      .end((err, res) => {
-          if (err || res.body.error || ( Object.keys(res.body).length === 0 ) ) {
-              console.log('Invalid Fob, bounties/:fob')
-              return isHandledCallback(false)
-          }
-
-          activeBounty = res.body
-          console.log("res to bounties/:fob", activeBounty)
-          let now = Date.now()
-          let monthValue = activeBounty.value
-          let lastClaimed = activeBounty.notes
-          let amount = calculatePayout(monthValue, lastClaimed, now)
-          // Build in the info we need from the bounty, next tap will send these requests
-          claimRequest.action["bounty-id"] = activeBounty["bounty-id"]
-          payoutRequest.action["notes"] = activeBounty["bounty-id"]
-          payoutRequest.action["amount"] = amount.toString()
-          // This was a bounty tag so we do not need to check for beer
-          isHandledCallback(true)
-      })
-  }
+        })
 }
+
+function bountyClaimProcess(scannedFob, isHandledCallback) {
+    bountyTagCheck(scannedFob, isHandledCallback)
+    if (activeBounty) {
+        // Have an active bounty so next tap is to claim bounty:
+        request
+            .get(config.brainLocation + 'members/' + scannedFob)
+            .end((err, res) => {
+                if (err || res.body.error) {
+                    console.log('Invalid Fob')
+                    // clear bounty if random fob tries to claim?
+                    resetBountyClaim()
+                    return isHandledCallback(false)
+                } else {
+                    claimant = res.body
+                    payoutRequest.action["address"] = claimant.address
+                    claimRequest.action["address"] = claimant.address
+                    claimRequest.action["notes"] = Date.now().toString()
+
+                    claimRequest()
+                    payoutRequest()
+                    slackReq()
+                    resetBountyClaim()
+                    isHandledCallback(true)
+                }
+            })
+    }
+}
+
+function claimRequest(){
+    request
+        .post(config.brainLocation + 'bounties')
+        .send(claimRequest)
+        .end((err, res) => {
+            if (err || res.body.error) {
+              console.log('Unable to create')
+            }
+            console.log(res.body)
+        })
+}
+
+function payoutRequest(){
+    request
+        .post(config.brainLocation + 'members')
+        .send(payoutRequest)
+        .end((err, res) => {
+            if (err || res.body.error) {
+              console.log('Unable to create')
+            }
+        })
+}
+
+function slackReq(){
+    request
+        .post(config.bountiesSlack)
+        .send({text: activeBounty.name + ' was claimed by ' + claimant.name+ ' for $'+ payoutRequest.action.amount})
+        .end( (err, res)=> {
+            console.log({err,res})
+        })
+}
+
 
 function calculatePayout(monthValue, lastClaimed, now){
     let msSince = now - lastClaimed
