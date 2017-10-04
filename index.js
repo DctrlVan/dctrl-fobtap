8ddbaa2f15734df7ab6edd219f3c8f9281eaa79b
rhodes <taylorsingletonfookes@live.com>
2017-10-04 16:12:51 -0700

    post location

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 
-
-// bountyClaimProcess(fob, isHandled => {
-//
-//     console.log("Should Try to charge for beer", {isHandled})
-//     if (!isHandled) {
-//         checkWithBrain(fob)
-//     }
-//     fob = ""
-// })
 require('./dispense')
-const config = require('./configuration')
+
 const request = require('superagent')
+const config = require('./configuration')
 const fobtapStream = require('./tap')
 // listen on the fob numbers from the reader and async chain bounty check then vend check
 fobtapStream
   .throttle(2345, {trailing: false})
   .onValue(fob => {
     request
-        .post(config.brainLocation + 'events/bounties_claim')
+        .post(config.brainLocation + 'fobtap')
         .send({
             fob,
             tapId: config.tapId
